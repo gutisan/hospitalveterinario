a6f2033c9eea11c00bec5140234dda37bd11442b
Gutisan <gutisan@users.noreply.github.com>
2024-07-09 00:08:55 +0200

    ca

--- src/main/java/com/example/hospitalveterinario/Holamundo.java
+++ src/main/java/com/example/hospitalveterinario/Holamundo.java
@@ -1,7 +1,7 @@
 package com.example.hospitalveterinario;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 public class Holamundo {
     @RequestMapping("/")
