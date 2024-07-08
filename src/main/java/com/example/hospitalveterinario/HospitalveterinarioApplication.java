380e24b0afffbbf9580d04afe6c098f416e39667
Gutisan <gutisan@users.noreply.github.com>
2024-07-08 23:30:12 +0200

    first commit

---
+++ src/main/java/com/example/hospitalveterinario/HospitalveterinarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalveterinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalveterinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalveterinarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hospitalveterinario/HospitalveterinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalveterinario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalveterinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
